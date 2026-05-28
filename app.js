dbe00415d45219d7e7cf896fb4afe39eec9ec59b
maeda-lab0423 <coconomaeda@gmail.com>
2026-05-28 23:43:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -550,9 +550,9 @@ if (clearSearchBtn) {
   if (navSearch) navSearch.addEventListener('click', showSearchScreen);
   if (navFavorites) navFavorites.addEventListener('click', showFavoritesScreen);
   if (navMypage) navMypage.addEventListener('click', showMyPageScreen);
-  if (saveRecordBtn) {
-  saveRecordBtn.addEventListener('click', addLiveRecord);
-}
+  //if (saveRecordBtn) {
+  //saveRecordBtn.addEventListener('click', addLiveRecord);
+//}
   if (changeStationBtn) {
   changeStationBtn.addEventListener('click', () => {
     stationDisplayArea.style.display = 'none';
